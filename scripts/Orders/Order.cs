b630a81d9a9a47142ac2d6d0b481dd9c7ce7178a
GEORGI_120 <mikaelan316@gmail.com>
2026-08-06 16:39:44 +0300

    Renames orders file; Clears unused orders code

---
+++ scripts/Orders/Order.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Orders;
+
+public class Description
+{
+	public byte Index { get; init; }
+	public sbyte DefenseChange { get; init; }
+	public byte SquadCount { get; init; } = 0;
+	public byte Duration { get; init; } = 1;
+	public byte MoveRange { get; init; }
+}
+
+public struct Data
+{
+	byte ExecutorId { get; set; }
+	byte Duration { get; set; }
+	byte Direction { get; set; } //(neighbour hex number) (direction sets to squad after execution)
+	byte TargetId { get; set; } //target (squad ID) (or hex num)
+								// hexes for moving to (maximum 4?)
+}
+
+public class Order
+{
+	public Description Description { get; init; }
+	public Action<Data> Execute { get; init; }
+}
