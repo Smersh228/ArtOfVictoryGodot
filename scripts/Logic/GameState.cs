9170933f7bda0859a1088e319e014f153089a0d0
GEORGI_120 <mikaelan316@gmail.com>
2026-08-10 19:35:09 +0300

    Adds pos to squads accessor; Reworks HexNum to axial coordinates (2 bytes)

--- scripts/Logic/GameState.cs
+++ scripts/Logic/GameState.cs
@@ -7,7 +7,7 @@ public record struct HexId(ushort Value);
 public record struct SquadId(ushort Value);
 public record struct OrderId(ushort Value);
 
-public record struct HexNum(ushort Value);
+public record struct HexNum(byte L1, byte L2);
 public record struct SquadNum(ushort Value);
 
 public record struct OrderCommand(SquadNum Executor, OrderId Order);
@@ -16,11 +16,13 @@ public readonly ref struct Squad
 {
 	public readonly Squads.Definition Def;
 	public readonly ref Squads.Data Data;
+	public readonly HexNum Pos;
 
-	public Squad(Squads.Definition def, ref Squads.Data data)
+	public Squad(Squads.Definition def, ref Squads.Data data, HexNum pos)
 	{
 		Def = def;
 		Data = ref data;
+		Pos = pos;
 	}
 }
 
@@ -28,7 +30,11 @@ public class GameState
 {
 	public Squad this[SquadNum num]
 	{
-		get => new(Registry.Squads[Squads[num.Value]], ref SquadsData[num.Value]);
+		get => new(
+			Registry.Squads[Squads[num.Value]],
+			ref SquadsData[num.Value],
+			Positions[num.Value]
+		);
 	}
 
 	public Registry Registry { get; init; }
@@ -37,7 +43,7 @@ public class GameState
 	public HexId[] Hexes { get; init; }
 	public SquadId[] Squads { get; init; }
 	public Squads.Data[] SquadsData { get; init; }
-	public ushort[] Positions { get; init; }
+	public HexNum[] Positions { get; init; }
 
 	//public Dictionary<Squads.Army>
 	public Queue<OrderCommand> Commands { get; init; } // 1sq = 1hod = 1order
